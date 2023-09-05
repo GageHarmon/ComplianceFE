50030201a6b377ac23147903aa1b1a99b8386192
Gage Harmon <gaharmon1@gmail.com>
2023-09-05 13:48:24 -0600

    created drop down to allow different report selection also working on miltple file load

--- complianceapp/app/ComplianceChecker.tsx
+++ complianceapp/app/ComplianceChecker.tsx
@@ -1,22 +1,34 @@
 'use client'
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const ComplianceChecker = () => {
-  const [file, setFile] = useState(null);
-  const [report, setReport] = useState(null);
+  const [files, setFiles] = useState<FileList | null>(null);
+  const [reportType, setReportType] = useState('backups');
+  const [report, setReport] = useState<any[]>([]);
 
   const handleFileChange = (e) => {
-    setFile(e.target.files[0]);
-  }
+    setFiles(e.target.files);
+  };
+
+  const handleReportTypeChange = (e) => {
+    setReportType(e.target.value);
+  };
 
   const handleSubmit = async () => {
-    if (!file) return;
+    if (!files) return;
 
     const formData = new FormData();
-    formData.append('csv', file);
+    Array.from(files).forEach((file, index) => {
+      formData.append(`csv${index + 1}`, file);
+    });
 
-    const response = await fetch('http://127.0.0.1:5000/upload', {
+    formData.append('reportType', reportType)
+
+    // URL changes depending on the selected report type
+    const url = reportType === "backups" ? 'http://127.0.0.1:5000/upload' : 'http://127.0.0.1:5000/other_report';
+
+    const response = await fetch(url, {
       method: 'POST',
       body: formData
     });
@@ -39,19 +51,23 @@ const ComplianceChecker = () => {
     } else {
         console.error("Unknown response type:", contentType);
     }
-}
+  };
 
   return (
     <div>
+      <select onChange={handleReportTypeChange}>
+        <option value="backups">Backups Audit</option>
+        <option value="sentinel">SentinelOne Audit</option>
+      </select>
       <input
         type="file"
+        multiple
         onChange={handleFileChange}
         className="border-2 border-gray-300 p-2 rounded-lg"
       />
       <button onClick={handleSubmit} className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-lg">
         Generate Report
       </button>
-
       {report && (
         <div className="mt-8">
           <h3 className="text-xl font-bold mb-4">Report:</h3>
@@ -65,7 +81,77 @@ const ComplianceChecker = () => {
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
 export default ComplianceChecker;
+
+// import { useState } from 'react';
+
+// const ComplianceChecker = () => {
+//   const [file, setFile] = useState(null);
+//   const [report, setReport] = useState(null);
+
+//   const handleFileChange = (e) => {
+//     setFile(e.target.files[0]);
+//   }
+
+//   const handleSubmit = async () => {
+//     if (!file) return;
+
+//     const formData = new FormData();
+//     formData.append('csv', file);
+
+//     const response = await fetch('http://127.0.0.1:5000/upload', {
+//       method: 'POST',
+//       body: formData
+//     });
+
+//     const contentType = response.headers.get("Content-Type");
+
+//     // Check if the response is a file
+//     if (contentType && contentType.startsWith("text/csv")) {
+//         const blob = await response.blob();
+//         const link = document.createElement('a');
+//         link.href = window.URL.createObjectURL(blob);
+//         link.download = "non_compliant_computers.csv";
+//         document.body.appendChild(link);
+//         link.click();
+//         document.body.removeChild(link);
+//     } else if (contentType && contentType.startsWith("application/json")) {
+//         // If not a file, assume it's a JSON error message
+//         const data = await response.json();
+//         console.error(data.error);  // Log the error or show it to the user
+//     } else {
+//         console.error("Unknown response type:", contentType);
+//     }
+// }
+
+//   return (
+//     <div>
+//       <input
+//         type="file"
+//         onChange={handleFileChange}
+//         className="border-2 border-gray-300 p-2 rounded-lg"
+//       />
+//       <button onClick={handleSubmit} className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-lg">
+//         Generate Report
+//       </button>
+
+//       {report && (
+//         <div className="mt-8">
+//           <h3 className="text-xl font-bold mb-4">Report:</h3>
+//           {/* Display the report (modify this based on your report structure) */}
+//           {report.map((item, index) => (
+//             <div key={index}>
+//               {/* Display some properties from the report for demonstration */}
+//               Device Name: {item.DeviceName}, Days Since Last Backup: {item.DaysSinceLastBackup}
+//             </div>
+//           ))}
+//         </div>
+//       )}
+//     </div>
+//   )
+// }
+
+// export default ComplianceChecker;
